5e1346c696c5f8d4f88029d8db9f16dc862640e7
Ben Richardson <benrichardson@bens-air.lan>
2023-06-13 21:31:44 -0500

    census api

---
+++ ben_richardson/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "c5f2de97da04c684ae2ec91dafcec322"
+
+# Geoapify API Key
+geoapify_key = "2d6a28567c3e460481d5a764c7756122"
+
+# Census API Key
+census_key = "216619952c105ff8514ad249b62e0513014ea8c8"
